08432b22455df59609e72db5d47882a7e59dacc5
apirahman <apirahman55@gmail.com>
2020-09-20 12:54:50 +0700

    First Commit

---
+++ assets/js/dashboard.js
@@ -0,0 +1,33 @@
+const more = document.getElementById("more");
+const dismiss = document.getElementById("dismiss");
+const sidebar = document.getElementById("sidebar");
+const sidebarOverlay = document.getElementById("sidebar-overlay");
+more.onclick = (e) => {
+    const sidebarActive = sidebar.className.match("active");
+    const sidebarOverlayActive = sidebarOverlay.className.match("active");
+
+    if (!sidebarActive && !sidebarOverlayActive) {
+        sidebar.classList.add("active");
+        sidebarOverlay.classList.add("active");
+    }
+};
+
+dismiss.onclick = (e) => {
+    const sidebarActive = sidebar.className.match("active");
+    const sidebarOverlayActive = sidebarOverlay.className.match("active");
+
+    if (sidebarActive && sidebarOverlayActive) {
+        sidebar.classList.remove("active");
+        sidebarOverlay.classList.remove("active");
+    }
+};
+
+sidebarOverlay.onclick = () => {
+    const sidebarActive = sidebar.className.match("active");
+    const sidebarOverlayActive = sidebarOverlay.className.match("active");
+
+    if (sidebarActive && sidebarOverlayActive) {
+        sidebar.classList.remove("active");
+        sidebarOverlay.classList.remove("active");
+    }
+};
